58c6a2f3c5e69628dfe7e39b808b4e38699350e9
VsalCode <vsal.ilham@gmail.com>
2025-05-15 10:27:50 +0700

    init: project

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function Add(num){
+    if(num < 10){
+      return setCount(num + 1) 
+    } else{
+      alert('Max 10!')
+    }
+  }
+  
+  function Reduce(num){
+     if(num > 0){
+      return setCount(num - 1) 
+    } else{
+      alert('Min 0!')
+    }       
+  }
+
+  return (
+    <>
+      <button onClick={() => Add(count) } >+</button>
+      <div>{count}</div>
+      <button onClick={() => Reduce(count) } >-</button>
+    </>
+  );
+}
+
+export default App;
